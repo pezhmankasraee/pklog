9a6634754c2990ab35acf26d7f51111c143cd6b7
pezhman <github@pezhmankasraee.com>
2025-06-06 13:37:50 +0200

    change module name to pklog
    	- go-pklog -> pklog

--- loglevel.go
+++ loglevel.go
@@ -1,14 +1,14 @@
 /**
 Copyright 2019 Pezhman Kasraee
 
-This file is part of go-pklog.
+This file is part of pklog.
 
-go-pklog is free software: you can redistribute it and/or modify
+pklog is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-go-pklog is distributed in the hope that it will be useful,
+pklog is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
